9b7f794988e661b603e193a41ba4371345dea7ec
Omowaye Gabriel <103774459+Oasomowaye@users.noreply.github.com>
2023-11-30 18:28:20 +0000

    main commit

--- src/Components/About.jsx
+++ src/Components/About.jsx
@@ -24,7 +24,7 @@ const imageAltText = "purple and blue abstract background";
  * Sort description that expands on your title on the Home component.
  */
 const description =
-  "I'm a UI/UX student studying at Barnett Technical University. I enjoy creating unique and simplistic user interfaces in creative ways.";
+  "I'm a Data Analyst and Statistics student studying at Ekiti State University. I enjoy analysis data and creating simplystic data dashboards and presentations in creative ways.";
 
 /**
  * List of some of skills or technologies you work on, are learning,
@@ -32,11 +32,11 @@ const description =
  */
 const skillsList = [
   "Web design",
-  "User experience",
-  "Inclusive design",
-  "Focus group testing",
-  "Mobile user interfaces",
-  "Graphic design",
+  "Data Analysis",
+  "Power BI",
+  "Excel",
+  "Python",
+  "SPSS"
 ];
 
 /**
@@ -45,7 +45,7 @@ const skillsList = [
  * about you on a professional level.
  */
 const detailOrQuote =
-  "I am passionate about solving problems in new creative ways to drive innovation. By leveraging my UI/UX experience I continually look for new and better ways to make tech accessible by all.";
+  "I am passionate about solving problems in new creative ways to drive innovation. By leveraging my data analysis experience I continually look for new and better ways to make tech accessible by all.";
 
 const About = () => {
   return (
